b296b9016b3d4ef84f15d74ddf8c51504af9e909
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-26 14:46:09 +0530

    first commit

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Constructor
+{
+    internal class Employee
+    {
+        int id;
+        string name;
+        public Employee()
+        {
+            Console.WriteLine("Employee Constructor");
+            id = -1;
+            name = "not Given";
+        }
+        public void Register()
+        {
+            Console.WriteLine("Enter ID");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+        }
+        public void Display()
+        {
+            Console.WriteLine("Id : "+id);
+            Console.WriteLine("Name : "+name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee employee2 = new Employee();
+            Console.WriteLine("Enter Employee One Details");
+            employee.Register();
+            Console.WriteLine("Enter Employee Two Details");
+            employee2.Register();
+            Console.WriteLine("Employee One Display");
+            employee.Display();
+            Console.WriteLine("Employee Two Display");
+            employee2.Display();
+        }
+    }
+}
